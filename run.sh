048a337891685d1ce58c427876419b991b094940
Simon Gottschlag <simon.gottschlag@xenit.se>
2021-06-16 22:58:35 +0200

    remove echo

--- run.sh
+++ run.sh
@@ -12,8 +12,6 @@ cleanup() {
     docker rm $(docker ps -a -f name=nginx-test -q) 1>/dev/null 2>&1
     docker stop $(docker ps -f name=opa-test -q) 1>/dev/null 2>&1
     docker rm $(docker ps -a -f name=opa-test -q) 1>/dev/null 2>&1
-    docker stop $(docker ps -f name=echo-test -q) 1>/dev/null 2>&1
-    docker rm $(docker ps -a -f name=echo-test -q) 1>/dev/null 2>&1
     docker stop $(docker ps -f name=opa-nginx-external-auth-test -q) 1>/dev/null 2>&1
     docker rm $(docker ps -a -f name=opa-nginx-external-auth-test -q) 1>/dev/null 2>&1
     docker network rm opaauthznginx 1>/dev/null 2>&1
@@ -30,11 +28,6 @@ echo "$(timestamp) [RUN] Docker network created"
 echo "$(timestamp) [RUN] Starting opa"
 docker run --network opaauthznginx -p 8181:8181 -v "$(pwd)"/opa:/mnt:ro --name opa-test -d openpolicyagent/opa:0.29.4-debug run --log-level info -c /mnt/opa.conf --server --addr :8181 /mnt 1>/dev/null 2>&1
 echo "$(timestamp) [RUN] Started opa"
-
-echo "$(timestamp) [RUN] Starting echo"
-docker build -t echo:dev -f echo/Dockerfile . 1>/dev/null 2>&1
-docker run --network opaauthznginx -p 8081:8081 --name echo-test -d echo:dev 1>/dev/null 2>&1
-echo "$(timestamp) [RUN] Started echo"
 
 echo "$(timestamp) [RUN] Starting opa-nginx-external-auth"
 docker build -t opa-nginx-external-auth:dev -f opa-nginx-external-auth/Dockerfile . 1>/dev/null 2>&1
